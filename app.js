21ff1ff4d70644c5d5d45d297165261d9591ead9
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-17 15:06:00 -0500

    array of objects

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ const adventurer = {
    {
     (console.log(adventurer.inventory[i]));
    }
+
+   const adventurer1 = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion:{
+            name: "Frank",
+            type: "Flea",
+            belongings: ["smallhat","sunglass"]
+        },
+        roll (mod = 0) {
+            const result = Math.floor(Math.random() * 20) + 1 + mod;
+            console.log(`${this.name} rolled a ${result}.`)}
+    }
+    }
+    adventurer1.roll();
