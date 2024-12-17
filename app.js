2adda2547a97d581dd9f208287ae703db758c3c9
Shobana Hariraman <shobanaravi2004@gmail.com>
2024-12-17 14:46:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+    }
+
+   for(let i=0;i<3;i++)
+   {
+    (console.log(adventurer.inventory[i]));
+   }
